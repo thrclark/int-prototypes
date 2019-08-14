4ce7c1c2e01d5f9443e595a472e2878ee8715abe
Tom Clark <thrclark@iu.edu>
2019-08-14 17:32:34 -0400

    test

--- php/template.php
+++ php/template.php
@@ -9,7 +9,7 @@ $page_title = 'Template';
 <?php include('includes/styles.php') ?>
 </head>
 <body>
-<h1> <?php echo $page_title; ?></h1>another test
+<h1> <?php echo $page_title; ?></h1> feature branch test
 <?php include('includes/template-include.php') ?>
 <?php include('modals/modal-template.php') ?>
 <?php include('includes/all-footerscripts.php') ?>
