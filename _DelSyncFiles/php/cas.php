e332493ca4849097dcdc5bfbcd3f370e95e1e914
Tom Clark <thrclark@iu.edu>
2019-08-19 18:58:56 -0400

    setup

---
+++ _DelSyncFiles/php/cas.php
@@ -0,0 +1,17 @@
+<?php 
+$section = '';
+$page_title = 'Template';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/all-head-meta.php') ?>
+<?php include('includes/styles.php') ?>
+</head>
+<body>
+<h1> <?php echo $page_title; ?></h1> feature branch test
+<?php include('includes/template-include.php') ?>
+<?php include('modals/modal-template.php') ?>
+<?php include('includes/all-footerscripts.php') ?>
+</body>
+</html>
---
+++ php/cas.php
@@ -0,0 +1,20 @@
+<?php 
+$section = '';
+$page_title = 'Template';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/all-head-meta.php') ?>
+<?php include('includes/styles.php') ?>
+</head>
+<body class="int-user">
+<main>
+    <header>
+        <h1> <?php echo $page_title; ?></h1>
+    </header>
+    <section> sdfg </section>
+</main>
+<?php include('includes/all-footerscripts.php') ?>
+</body>
+</html>
