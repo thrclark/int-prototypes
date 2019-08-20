0228760b49320e951418adc556ead0d1ae11d34d
Tom Clark <thrclark@iu.edu>
2019-08-20 15:39:39 -0400

    INT-289

--- php/cas.php
+++ php/cas.php
@@ -1,6 +1,6 @@
 <?php 
 $section = '';
-$page_title = 'Template';
+$page_title = 'LOGIN';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +9,38 @@ $page_title = 'Template';
 <?php include('includes/styles.php') ?>
 </head>
 <body class="int-user">
+<header> <img alt="" class="int-brand-image">
+    <div class="int-lockup-group">
+        <div class="int-lockup-org">INDIANA UNIVERSITY</div>
+        <h1 class="int-lockup-pagetitle"><?php echo $page_title; ?></h1>
+    </div>
+</header>
 <main>
-    <header>
-        <h1> <?php echo $page_title; ?></h1>
-    </header>
-    <section> sdfg </section>
+    <section>
+        <div class="card">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" aria-describedby="username" >
+                </div>
+                <div class="form-group">
+                    <label for="passphrase">Passphrase</label>
+                    <input type="password" class="form-control" id="passphrase" aria-describedby="passphrase" >
+                </div>
+                <button type="button" class="btn btn-primary btn-block mt-6">Log in</button>
+                <div class="int-help"> <a href="https://kb.iu.edu/d/auog" target="_blank" class="mr-5"><i class="rbt-icon-info-circle"></i> About the new look</a> <a href="https://kb.iu.edu/d/alpu" target="_blank"><i class="rbt-icon-question-mark"></i> Get help</a></div>
+            </div>
+        </div>
+    </section>
 </main>
+
+<!--Custom footer-->
+
+<footer class="custom-footer">
+    <p> <a href="https://www.iu.edu/copyright/index.html" target="_blank" rel="noopener noreferrer">Copyright</a> © 2019 The Trustees of <a href="http://www.iu.edu/" target="_blank" rel="noopener noreferrer">Indiana University</a>. </p>
+</footer>
+
+<!--<?php include('includes/theme-controls.php') ?>-->
 <?php include('includes/all-footerscripts.php') ?>
 </body>
 </html>
--- php/includes/all-footerscripts.php
+++ php/includes/all-footerscripts.php
@@ -2,3 +2,106 @@
 <script src="../js/popper.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 
+
+
+<script>
+    const setValue = (property, value) => {
+        if (value) {
+            document.documentElement.style.setProperty(`--${property}`, value);
+
+            const input = document.querySelector(`#${property}`);
+            if (input) {
+                value = value.replace('px', '');
+                input.value = value;
+            }
+        }
+    };
+
+const setValueFromLocalStorage = property => {
+    let value = localStorage.getItem(property);
+    setValue(property, value);
+};
+
+const setTheme = options => {
+    for (let option of Object.keys(options)) {
+        const property = option;
+        const value = options[option];
+        setValue(property, value);
+        localStorage.setItem(property, value);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    setValueFromLocalStorage('logo-image-url');
+   
+
+
+});
+
+const dataThemeButtons = document.querySelectorAll('[data-theme]');
+
+for (let i = 0; i < dataThemeButtons.length; i++) {
+    dataThemeButtons[i].addEventListener('click', () => {
+        const theme = dataThemeButtons[i].dataset.theme;
+
+        switch (theme) {
+            case 'brown':
+                setTheme({
+
+             
+                    'logo-image-url': "url('https://is.wfu.edu/wp-content/uploads/2017/01/cropped-favicon.png')",
+                    
+
+                });
+                return;
+
+            case 'blue':
+                setTheme({
+                    'logo-image-visibility': 'block',
+
+                    'logo-image-url': "url('https://cdn.freebiesupply.com/logos/large/2x/villanova-wildcats-logo-png-transparent.png')",
+                   
+                });
+                return;
+
+            case 'yellow':
+                setTheme({
+                   
+
+                    'logo-image-url': "url('http://www.indiana.edu/~espd/rvb/img/webster.png')",
+                   
+                });
+                return;
+
+            case 'black':
+                setTheme({
+                   
+                    'logo-image-url': "url('http://www.indiana.edu/~espd/rvb/develop/img/trident-large.png')",
+                  
+                });
+                return;
+
+            case 'white':
+                setTheme({
+                   
+                    'logo-image-url': "url('http://www.indiana.edu/~espd/rvb/develop/img/trident-large.png')",
+                    
+
+                });
+                return;
+        }
+    })
+}
+
+const handleInputChange = (property, pixels) => {
+    document.documentElement.style.setProperty(`--${property}`, `${event.target.value}${pixels ? 'px' : ''}`);
+    localStorage.setItem(property, `${event.target.value}${pixels ? 'px' : ''}`);
+};
+
+document.querySelector('#logo-image-url').addEventListener('input', event => {
+    handleInputChange('logo-image-url', false);
+});
+
+
+</script>
