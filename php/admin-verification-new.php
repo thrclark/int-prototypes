2b19d48d627c49c4d8cf5f7dd5adbe9869c2f7e6
Tom Clark <thrclark@iu.edu>
2019-09-13 09:05:28 -0400

    INT-291

--- php/admin-verification-new.php
+++ php/admin-verification-new.php
@@ -13,12 +13,12 @@ $page_title = 'New verification';
 <div class="container mt-5 mb-5">
     <main class="main-content" id="main-content">
         <h1> <?php echo $page_title; ?></h1>
-        <ol class="rvb-steps">
-            <li class="rvb-steps__item"> <a href="#" class="rvb-steps__item-content" aria-current="step"> <span class="rvb-steps__label">Setup</span> <span class="rvb-steps__indicator"> <span class="sr-only">Step</span> 1 </span> </a> </li>
-            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Data</span> <span class="rvb-steps__indicator"> <span class="sr-only">Step</span> 2 </span> </span> </li>
-            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Recipients</span> <span class="rvb-steps__indicator"> <span class="sr-only">Step</span> 3 </span> </span> </li>
-            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Scheduling</span> <span class="rvb-steps__indicator"> <span class="sr-only">Step</span> 4 </span> </span> </li>
-            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Review/Save</span> <span class="rvb-steps__indicator"> <span class="sr-only">Step</span> 5 </span> </span> </li>
+        <ol class="rvb-steps rvb-steps--vertical">
+            <li class="rvb-steps__item"> <a href="#" class="rvb-steps__item-content" aria-current="step"> <span class="rvb-steps__label">Setup</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-gear"></i></span> </a> </li>
+            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Data</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-clipboard"></i></span> </span> </li>
+            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Recipients</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-users-group-solid"></i> </span> </span> </li>
+            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Scheduling</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-calendar"></i></span> </span> </li>
+            <li class="rvb-steps__item"> <span class="rvb-steps__item-content"> <span class="rvb-steps__label">Review/Save</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-eye"></i> </span> </li>
         </ol>
     </main>
 </div>
