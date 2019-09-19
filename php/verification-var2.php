b3aba3788ca00d83d95501e827ce061a39c9d470
Tom Clark <thrclark@iu.edu>
2019-09-19 10:16:29 -0400

    INT-289

---
+++ php/verification-var2.php
@@ -0,0 +1,177 @@
+<?php 
+$section = '';
+$page_title = 'VERIFY';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/all-head-meta.php') ?>
+<?php include('includes/styles-main.php') ?>
+</head>
+<body class="int-user">
+<header class="container p-0 mt-7 mb-3">
+    <div class="d-flex"> <img alt="" class="int-brand-image">
+        <div class="int-lockup-group flex-grow-1">
+            <div class="int-lockup-org">INDIANA UNIVERSITY</div>
+            <h1 class="int-lockup-pagetitle"><?php echo $page_title; ?></h1>
+        </div>
+        <div class="d-none d-lg-block">
+            <div class="rbt-header-avatar-id"> <span class="rbt-header-avatar" aria-hidden="true">TC</span> thrclark </div>
+        </div>
+    </div>
+</header>
+<main>
+    <section>
+        <div class="alert alert-warning mb-5" role="alert">
+            <h2 class="alert-heading">Privacy note</h2>
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3"> This page contains personally sensitive data which is masked to protect your privacy.</div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-sm btn-primary demojs-reveal">Unmask sensitive data</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3"> You can choose to verify later. You may choose this option 3 more time(s).</div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-sm btn-outline-primary">Verify later</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="ts-26">Verify your data</h2>
+                <p> On occasion, we require verification of your personal information. The item(s) listed below need your review for accuracy.</p>
+                <p>This is an additional, optional paragraph that may be customized as needed. This paragraph may include <a href="#">links</a>. Esse errem aeterno id, his ad blandit imperdiet. Id unum propriae suavitate sea. Adipisci delicata ocurreret ad qui. Altera impedit expetendis vis at.</p>
+                <h3 class="ts-26 mt-5">IU Notify</h3>
+                <p>Displayed below is the contact information on file for you. Please review and update or correct it is to receive critical alerts from IU. For more information, see about emergency notifications at Indiana University.</p>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <dl class="row">
+                            <dt class="col-sm-4">IU Office</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;"> 812-855-5555</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Home</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">812-336-0510</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Secondary Home</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">Not Specified</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Mobile 1</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">812-369-9664</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Mobile 2</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">Not Specified</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Mobile 3</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">Not Specified</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                        </dl>
+                        <button class="btn btn-sm btn-outline-primary">Edit</button>
+                    </li>
+                    <li class="list-group-item">
+                        <dl class="row">
+                            <dt class="col-sm-4">Non-IU Email</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">thrclark@gmail.com</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                            <dt class="col-sm-4">Recieve notices from</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">IU Bloomington<br>
+                                IU Kokomo</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                        </dl>
+                        <button class="btn btn-sm btn-outline-primary">Edit</button>
+                    </li>
+                </ul>
+                <h3 class="ts-26 mt-5">Military Status</h3>
+                <p>Indiana University periodically invites all employees to update their veteran status. At this time, we ask all employees to review their current status below and click edit if this information is incorrect. Definitions of each veteran category can be seen by clicking the edit button. Data collected will be used for reporting and analysis purposes and will not be used against you in anyway.</p>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <dl class="row">
+                            <dt class="col-sm-4">Military Status</dt>
+                            <dd class="col-sm-8"><span class="int-privatedata" style="display:none;">Not a Veteran</span> <span class="int-mask"> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> </span> </dd>
+                        </dl>
+                        <button class="btn btn-sm btn-outline-primary">Edit</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="int-verify-controls p-3">
+            <p class="">This is an optional paragraph that can be customized. Abundantly made days herb land called whose. Won't a night. Third hath greater face replenish whales fruit saying is meat. Us bearing. Air day called Also, abundantly forth she'd night may. They're gathered you're place fifth. Fowl they're saw midst for waters kind and make green and winged sea their whales you'll them seas set i days first itself open. </p>
+            <hr>
+            <div class="row mb-5" id="demojs_revealskipscreen">
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3 ts-20"> This page contains personally sensitive data which is masked to protect your privacy.</div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-primary btn-block demojs-reveal" onclick='window.scrollTo({top: 0, behavior: "smooth"});'>Unmask sensitive data</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3 ts-20"> You can choose to verify later. You may choose this option 3 more time(s).</div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-outline-primary btn-block">Verify later</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5" id="demojs_verifyscreen" style="display:none">
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3 ts-20">
+                            <div class="form-check form-check-inline">
+                                <input id="question4" name="checked" type="checkbox" aria-invalid="false" data-container="body" data-toggle="popover" data-placement="top" data-content="Required" data-original-title="" title="">
+                                <label class="form-check-label" for="question4"> I have verified that the above information is up to date and correct.</label>
+                            </div>
+                        </div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-primary btn-block" id="submit2">Verify</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="d-flex flex-column">
+                        <div class="mb-3 ts-20"> You can choose to verify later. You may choose this option 3 more time(s).</div>
+                        <div class="rbt-button-group mt-auto">
+                            <button type="button" class="btn btn-outline-primary btn-block">Verify later</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include('includes/custom-footer.php') ?>
+<?php include('includes/theme-controls.php') ?>
+<?php include('includes/all-footerscripts.php') ?>
+<script>
+    $(".demojs-reveal").click(function() {
+        $(".int-mask").hide();
+		$(".alert").hide();
+		$("#demojs_revealskipscreen").hide();
+		$("#demojs_verifyscreen").show();
+        $(".int-privatedata").show();	
+    });
+</script> 
+<script>
+    $(document).ready(function() {
+        $("#submit2").click(function() {
+            $('#question4').popover('toggle')
+        });
+		
+		 $("#question4").click(function() {
+			 $('#question4').popover('show')
+            $('#question4').popover('destroy')
+        });
+		
+		 $('#submit2').click(function() {
+            if ($('#question4').is(':checked')) {
+				 $('#question4').popover('destroy')
+            }
+        });
+		
+		
+		
+		
+    }); 
+</script>
+</body>
+</html>
