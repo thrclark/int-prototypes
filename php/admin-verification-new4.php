1d690ba5bc1b33c7a6777abc6119c51e4fed0f1b
Tom Clark <thrclark@iu.edu>
2019-10-01 13:26:06 -0400

    INT-291

--- php/admin-verification-new4.php
+++ php/admin-verification-new4.php
@@ -15,7 +15,7 @@ $page_subtitle = 'Leeway';
     <main class="main-content" id="main-content">
         <h1 class="mb-3 mb-md-6 rbt-ts-23 rbt-ts-32-md-up"> <?php echo $page_title; ?></h1>
         <div class="row">
-            <div class="col-2 border-right">
+            <div class="col-2 border-right d-none d-sm-block">
                 <div id="sticky">
                     <ol class="rvb-steps rvb-steps--vertical mb-5" style="overflow-x:inherit">
                         <li class="rvb-steps__item"> <a href="admin-verification-new1.php" class="rvb-steps__item-content"> <span class="rvb-steps__label  d-none d-lg-block">Setup</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-gear"></i></span> </a> </li>
@@ -27,8 +27,16 @@ $page_subtitle = 'Leeway';
                     </ol>
                 </div>
             </div>
-            <div class="col-10 pl-5">
+            <div class="col-12 col-sm-10 pl-sm-5">
                 <h2 class="mb-3 rbt-ts-20 rbt-ts-26-md-up"><?php echo $page_subtitle; ?></h2>
+                <ol class="rvb-steps mb-5 d-sm-none">
+                    <li class="rvb-steps__item"> <a href="admin-verification-new1.php" class="rvb-steps__item-content"> <span class="rvb-steps__label"><span class="sr-only">Setup</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-gear"></i> </span> </a> </li>
+                    <li class="rvb-steps__item"> <a href="admin-verification-new2.php" class="rvb-steps__item-content"> <span class="rvb-steps__label"><span class="sr-only">Data</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-clipboard"></i> </span> </a> </li>
+                    <li class="rvb-steps__item"> <a href="admin-verification-new3.php" class="rvb-steps__item-content"> <span class="rvb-steps__label"><span class="sr-only">Recipients</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-users-group-solid"></i> </span> </a> </li>
+                    <li class="rvb-steps__item"> <a href="admin-verification-new4.php" class="rvb-steps__item-content"  aria-current="step"> <span class="rvb-steps__label"><span class="sr-only">Leeway</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-clock"></i> </span> </a> </li>
+                    <li class="rvb-steps__item"> <a href="admin-verification-new5.php" class="rvb-steps__item-content"> <span class="rvb-steps__label"><span class="sr-only">Scheduling</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-calendar"></i> </span> </a> </li>
+                    <li class="rvb-steps__item"> <a href="admin-verification-new6.php" class="rvb-steps__item-content" > <span class="rvb-steps__label"><span class="sr-only">Review/save</span></span> <span class="rvb-steps__indicator"> <i class="rbt-icon-eye"></i> </span> </a> </li>
+                </ol>
                 <div class="row">
                     <div class="col-12 col-lg-12">
                         <div class="form-group mb-5">
