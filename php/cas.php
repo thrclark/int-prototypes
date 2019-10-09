3f501b27a5178858c9f6b070c71c950addda83d7
Tom Clark <thrclark@iu.edu>
2019-10-09 14:13:06 -0400

    link fix

--- php/cas.php
+++ php/cas.php
@@ -30,7 +30,7 @@ $page_title = 'LOGIN';
                     <label for="passphrase">Passphrase</label>
                     <input type="password" class="form-control" id="passphrase" aria-describedby="passphrase" >
                 </div>
-                <button type="button" class="btn btn-primary btn-block mt-6" onclick="location.href='verification.php';">Log in</button>
+                <button type="button" class="btn btn-primary btn-block mt-6" onclick="location.href='verification-var1.php';">Log in</button>
                 <div class="int-help"> <a href="https://kb.iu.edu/d/auog" target="_blank" class="mr-5"><i class="rbt-icon-info-circle"></i> About the new look</a> <a href="https://kb.iu.edu/d/alpu" target="_blank"><i class="rbt-icon-question-mark"></i> Get help</a></div>
             </div>
         </div>
