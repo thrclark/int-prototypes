ef6f7e0847af2a484f4f7b09539452105b8896c4
Tom Clark <thrclark@iu.edu>
2019-10-22 14:58:20 -0400

    INT-296

--- php/admin-settings-apiaccounts-new.php
+++ php/admin-settings-apiaccounts-new.php
@@ -30,7 +30,7 @@ $page_title = 'New API account';
                     <label class="control-label" id="nameLabel"> Status </label>
                     <div> The availability of this API Account. </div>
                     <div class="row">
-                        <div class="col-12 col-md-4 col-lg-3">
+                        <div class="col-12 col-md-6 col-lg-4">
                             <select aria-labelledby="statusLabel statusDirections" class="form-control" aria-invalid="false">
                                 <option label="Active" value="string:ACTIVE" selected="selected">Active</option>
                                 <option label="Inactive" value="string:INACTIVE">Inactive</option>
@@ -39,32 +39,71 @@ $page_title = 'New API account';
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label" id="nameLabel"> API Account Type </label>
+                    <label class="control-label" for="account_type"> API Account Type </label>
                     <div> Maintenance accounts are used for maintaining resources within the application. External accounts are used to connect to external applications for data.</div>
                     <div class="row">
-                        <div class="col-12 col-md-4 col-lg-3">
-                            <select aria-labelledby="statusLabel statusDirections" class="form-control" aria-invalid="false">
-                                <option label="Active" value="string:ACTIVE" selected="selected">External API</option>
-                                <option label="Inactive" value="string:INACTIVE">Maintenance API</option>
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <select aria-labelledby="statusLabel statusDirections" class="form-control" id="account_type">
+                                <option value="external_api" selected="selected">External API</option>
+                                <option value="maintenance_api">Maintenance API</option>
                             </select>
                         </div>
                     </div>
+                </div>
+                <div class="demojs-externalapi">
+                    <div class="font-weight-bold ts-14 mb-2">External Rest API URLs</div>
+                    <div class="rbt-collapse-accordion" id="accordion">
+                        <div class="card">
+                            <h3 class="mb-2" id="heading_url1">
+                                <button class="rbt-btn-nostyle btn-block" type="button" data-toggle="collapse" data-target="#collapse_url1" aria-expanded="false" aria-controls="collapse_url1"> <i class="rbt-icon-chevron-right"></i>URL 1 </button>
+                            </h3>
+                            <div id="collapse_url1" class="collapse" aria-labelledby="heading_url1" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="url1_name">Name</label>
+                                        <div>This will be displayed in Verification publishing so you can identify this endpoint URL in order to know what to select.</div>
+                                        <div class="row">
+                                            <div class="col-12 col-sm-8 col-md-6">
+                                                <input type="text" class="form-control" id="url1_name" >
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="url1_url">URL</label>
+                                        <div>External URL to connect to retrieve data from.  Use the syntax {attributeName} for where you want the user data to be put in your URL (e.g https://example.com/{personId} or https://example.com?user={userName}.  Please only use one of these fields in your URL (userName, personId, emails).  These map up with your attributes (sAMAccountName, iuEduPSEMPLID, mail). </div>
+                                        <div class="row">
+                                            <div class="col-12 col-sm-8 col-md-6">
+                                                <input type="text" class="form-control" id="url1_url" >
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="textinput-full">Select Input</label>
+                                        <div class="row">
+                                            <div class="col-12 col-sm-8 col-md-6">
+                                                <select class="form-control" id="exampleFormControlSelect1">
+                                                    <option selected="selected">Choose an option...</option>
+                                                    <option>Option one</option>
+                                                    <option>Option two</option>
+                                                    <option>Option three</option>
+                                                    <option>Option four</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-sm btn-outline-primary"> <i class="rbt-icon-plus"></i> URL</button>
                 </div>
                 <br>
                 <br>
                 <br>
                 <br>
                 <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <div class="form-group demojs-externalapi">
-					
-					  <label class="control-label" for="publishers"> Publishers </label>
-					
-					
+                <div class="form-group">
+                    <label class="control-label" for="publishers"> Publishers </label>
                     <select multiple="multiple" id="publishers" class="form-control">
                         <option value='elem_1'>Item 1</option>
                         <option value='elem_2'>Item 2</option>
@@ -72,7 +111,6 @@ $page_title = 'New API account';
                         <option value='elem_4'>Item 4</option>
                     </select>
                 </div>
-                <br>
                 <br>
                 <br>
                 <br>
@@ -113,6 +151,19 @@ $page_title = 'New API account';
 
 		$('#publishers').multiSelect()
     });
+</script> 
+<script>
+$(document).ready(function() {
+    $(function() {
+        $('#account_type').change(function() {
+            if ($('#account_type').val() == 'external_api') {
+                $('.demojs-externalapi').show();
+            } else {
+                $('.demojs-externalapi').hide();
+            }
+        });
+    });
+});
 </script>
 </body>
 </html>
