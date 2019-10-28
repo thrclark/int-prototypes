024709bb32b1247cf661284968ce79ee287c8601
Tom Clark <thrclark@iu.edu>
2019-10-28 10:06:10 -0400

    INT-306

--- php/admin-admin-ga.php
+++ php/admin-admin-ga.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $section = '';
 $page_title = 'Google Analytics';
 ?>
@@ -13,8 +13,57 @@ $page_title = 'Google Analytics';
 <div class="container mt-3 mt-md-5 mb-5">
     <main class="main-content" id="main-content">
         <h1 class="mb-3 mb-md-6 rbt-ts-23 rbt-ts-32-md-up"> <?php echo $page_title; ?></h1>
+        <div class="row">
+            <div class="col-12 col-lg-8">
+                <div class="form-group">
+                    <label class="control-label" for="ldap_url"> Google Analytics Rollup ID</label>
+                    <div>Specify a Google Analytics ID to which tenant data will be submitted.</div>
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-4">
+                            <input value="UA-39645822-17" aria-labelledby="" class="form-control" id="ldap_url" type="text">
+                        </div>
+                    </div>
+                </div>
+                <div class="font-weight-bold ts-14"> Included Tenants</div>
+                <div class="mb-3">Select the tenants that will submit data to the specified Google Analytics ID.</div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="form-check-label" for="defaultCheck1">Classifieds.IU STG </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                    <label class="form-check-label" for="defaultCheck2">Classifieds.IU STG</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                    <label class="form-check-label" for="defaultCheck3">Classifieds.IU PRD</label>
+                </div>
+                <hr>
+                <div class="rbt-button-group mt-3 mb-5" id="savePublisher">
+                    <button class="btn btn-primary" id="submitform"> <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span> <span class="visible">Save</span> </button>
+                </div>
+            </div>
+        </div>
     </main>
 </div>
 <?php include('includes/all-footerscripts.php') ?>
+<script>
+    $(document).ready(function() {
+        $(function() {
+            $("#submitform").click(function() {
+                $('.btn.btn-primary').addClass('rbt-loading');
+				$('.btn.btn-primary .spinner-border').removeClass('d-none');
+				$('.btn.btn-primary .visible').toggleClass('invisible');
+				$("#submitform").prop('disabled', true);
+                setTimeout(function() {
+                    $('.btn.btn-primary').removeClass('rbt-loading');
+					$('.btn.btn-primary .spinner-border').addClass('d-none');
+					$("#submitform").prop('disabled', false);
+					$('.btn.btn-primary .invisible').removeClass('invisible');
+                }, 2000)
+            });
+        });
+    });
+</script>
 </body>
 </html>
