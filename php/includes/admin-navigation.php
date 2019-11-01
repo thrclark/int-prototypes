7a99e0b2b4c0e64d936bf4311cfadd6b05d653bf
thrclark <thrclark@users.noreply.github.com>
2019-11-01 12:23:48 -0400

    INT-307

--- php/includes/admin-navigation.php
+++ php/includes/admin-navigation.php
@@ -9,7 +9,7 @@
             </li>
             <li class="nav-item dropdown">
                 <button class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</button>
-                <div class="dropdown-menu"> <a class="dropdown-item" href="admin-settings-genereal.php">General Settings </a> <a class="dropdown-item" href="admin-settings-apiaccounts.php">API Accounts </a> <a class="dropdown-item" href="admin-settings-publishers.php">Publishers</a> <a class="dropdown-item" href="admin-settings-viewreset.php">View/Reset User Verifications </a> <a class="dropdown-item" href="admin-settings-ldap.php">LDAP Configuration </a></div>
+                <div class="dropdown-menu"> <a class="dropdown-item" href="admin-settings-general.php">General Settings </a> <a class="dropdown-item" href="admin-settings-apiaccounts.php">API Accounts </a> <a class="dropdown-item" href="admin-settings-publishers.php">Publishers</a> <a class="dropdown-item" href="admin-settings-viewreset.php">View/Reset User Verifications </a> <a class="dropdown-item" href="admin-settings-ldap.php">LDAP Configuration </a></div>
             </li>
             <li class="nav-item"> <a class="nav-link  " href="admin-customize.php">Customize</a> </li>
             <li class="nav-item dropdown">
@@ -34,7 +34,7 @@
             </li>
             <li class="list-group-item rbt-collapsable"> <a href="#" class="collapsed"  data-toggle="collapse" data-target="#nav_settings">Settings</a>
                 <ul class="collapse" id="nav_settings">
-                    <li class="list-group-item"> <a href="admin-settings-genereal.php">General Settings </a> </li>
+                    <li class="list-group-item"> <a href="admin-settings-general.php">General Settings </a> </li>
                     <li class="list-group-item"> <a href="admin-settings-apiaccounts.php">API Accounts </a> </li>
                     <li class="list-group-item"> <a href="admin-settings-publishers.php">Publishers</a> </li>
                     <li class="list-group-item"> <a href="admin-settings-viewreset.php">View/Reset User Verifications </a> </li>
