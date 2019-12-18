1d8abf2e2aa09a8d020ba2a6017e0a7bebdc7642
thrclark <thrclark@users.noreply.github.com>
2019-12-18 09:56:30 -0500

    INT-294

--- php/admin-settings-apiaccounts.php
+++ php/admin-settings-apiaccounts.php
@@ -36,9 +36,9 @@ $page_title = 'API Accounts';
             </thead>
             <tbody>
                 <tr>
-                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle mr-2 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle mr-2 ts-12 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Name</b> <span class="rbt-table-responsive-cell-content ">Citizenship Status API</span></td>
-                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External  </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Expires</b> <span class="rbt-table-responsive-cell-content ">---</span></td>
                     <td><b class="rbt-table-responsive-cell-label">Email</b> <span class="rbt-table-responsive-cell-content ">thrclark@iu.edu</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
@@ -50,9 +50,9 @@ $page_title = 'API Accounts';
                         </div></td>
                 </tr>
                 <tr>
-                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-lightning mr-2" style="color: #CCCCCC" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Inactive"></i> </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle-empty mr-2 ts-12 text-muted" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Inactive"></i>  </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Name</b><span class="tablesaw-cell-content ng-binding">HR Personal Information</span></td>
-                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External  </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Expires</b> <span class="rbt-table-responsive-cell-content ">---</span></td>
                     <td><b class="rbt-table-responsive-cell-label">Email</b> <span class="rbt-table-responsive-cell-content ">naiursd@iu.edu</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
@@ -65,9 +65,9 @@ $page_title = 'API Accounts';
                         </div></td>
                 </tr>
                 <tr>
-                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-lightning mr-2 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle mr-2 ts-12 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i></span></td>
                     <td><b class="rbt-table-responsive-cell-label">Name</b> <span class="rbt-table-responsive-cell-content ">IU Notify Live Data</span></td>
-                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External  </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">External </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Expires</b> <span class="rbt-table-responsive-cell-content ">---</span></td>
                     <td><b class="rbt-table-responsive-cell-label">Email</b> <span class="rbt-table-responsive-cell-content ">tharoh@iu.edu</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
@@ -80,9 +80,9 @@ $page_title = 'API Accounts';
                         </div></td>
                 </tr>
                 <tr>
-                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-lightning mr-2 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> <i class="rbt-icon-lock-closed" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Locked"></i></span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle mr-2 ts-12 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> <i class="rbt-icon-lock-closed" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Locked"></i></span></td>
                     <td><b class="rbt-table-responsive-cell-label">Name</b> <span class="rbt-table-responsive-cell-content ">IU Notify Maintenance API</span></td>
-                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">Maintenance  </span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">Maintenance </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Expires</b> <span class="rbt-table-responsive-cell-content ">Mar 20, 2020</span></td>
                     <td><b class="rbt-table-responsive-cell-label">Email</b> <span class="rbt-table-responsive-cell-content ">yjalek@iu.edu</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
@@ -95,7 +95,7 @@ $page_title = 'API Accounts';
                         </div></td>
                 </tr>
                 <tr>
-                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-lightning mr-2 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> <i class="rbt-icon-lock-open" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Unlocked"></i></span></td>
+                    <td><b class="rbt-table-responsive-cell-label">Status</b> <span class="rbt-table-responsive-cell-content "><i class="rbt-icon-circle mr-2 ts-12 text-success" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Active"></i> <i class="rbt-icon-lock-open" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Unlocked"></i></span></td>
                     <td><b class="rbt-table-responsive-cell-label">Name</b> <span class="rbt-table-responsive-cell-content ">TAM PRD Notifications</span></td>
                     <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">Maintenance </span></td>
                     <td><b class="rbt-table-responsive-cell-label">Expires</b> <span class="rbt-table-responsive-cell-content ">Mar 20, 2020</span></td>
@@ -111,282 +111,6 @@ $page_title = 'API Accounts';
                 </tr>
             </tbody>
         </table>
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-<ul class="list-unstyled form-row demo-icons">
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-star"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-star-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-home"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-eye"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-ban"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-alarm"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-arrow-down"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-arrow-left"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-arrow-right"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-arrow-up"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-audio-off"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-audio-on"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-bell"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-building"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-calendar"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chat"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-check"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-check-all"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-check-circle-breakout"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-check-circle"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevron-down"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevron-left"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevron-right"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevron-up"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevrons-left"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-chevrons-right"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-clipboard"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-clock"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-close"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-close-circle"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-close-circle-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-code"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-copy"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-download"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-ellipsis"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-ellipsis-vertical"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-envelope"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-exclamation"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-file"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-flag"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-gear"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-grid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-info-circle"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-lightning"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-lightning-box"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-link-external"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-list"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-lock-closed"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-lock-open"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-magnifying-glass"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-map-pin"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-menu"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-minus"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-minus-circle"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-note"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-pencil"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-phone"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-pin"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-plus"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-plus-circle"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-printer"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-question-mark"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-redo"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-save"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-settings"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-sync"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-transfer"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-transfer-alternate"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-trash"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-filter"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-undo"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-upload"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user-add"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user-add-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user-remove"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user-remove-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-user-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-users"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-users-group-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-users-solid"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-gears"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-rss"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-play"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-pause"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-circle-empty"></i></div>
-    </li>
-    <li class="col-auto">
-        <div class="border"><i class="rbt-icon-circle"></i></div>
-    </li>
-</ul>
     </main>
 </div>
 <?php include('includes/all-footerscripts.php') ?>
