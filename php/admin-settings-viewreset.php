e84de5d1c4f6435091b082617d42597ca500c8aa
thrclark <thrclark@users.noreply.github.com>
2020-01-09 09:55:20 -0500

    INT-313

--- php/admin-settings-viewreset.php
+++ php/admin-settings-viewreset.php
@@ -13,11 +13,11 @@ $page_title = 'Reset user verifications';
 <div class="container mt-3 mt-md-5 mb-5">
     <main class="main-content" id="main-content">
         <h1 class="mb-3 mb-md-6 rbt-ts-23 rbt-ts-32-md-up"> <?php echo $page_title; ?></h1>
-        <div class="row mb-5">
-            <div class="col-12 col-lg-8">
-                <div class="form-group">
-                    <label class="control-label" for="userSearch"> Search </label>
-                    <div> You may reset verifications for a specific user by searching user name, ID, or email address.</div>
+        <div class="form-group">
+            <label class="control-label" for="userSearch"> Search </label>
+            <div class="mb-2"> You may reset verifications for a specific user by searching user name, ID, or email address.</div>
+            <div class="row mb-5">
+                <div class="col-12 col-lg-6">
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Search user name, ID, or email" id="userSearch">
                         <div class="input-group-append">
@@ -30,12 +30,11 @@ $page_title = 'Reset user verifications';
         <h2 class="mb-3 rbt-ts-20 rbt-ts-26-md-up demojs-searchresults" style="display: none">Search results for 'thrclark'</h2>
         <table class="table rbt-table-responsive rbt-table-actions demojs-searchresults" style="display: none">
             <thead>
-                <tr>
-                    <th>Verification name</th>
-                    <th>Transaction date</th>
-                    <th>Type</th>
-                    <th><span class="sr-only">Actions</span></th>
-                </tr>
+            <th>Verification name</th>
+                <th>Transaction date</th>
+                <th>Type</th>
+                <th><span class="sr-only">Actions</span></th>
+            </tr>
             </thead>
             <tbody>
                 <tr>
@@ -44,7 +43,7 @@ $page_title = 'Reset user verifications';
                     <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">Verify</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
                         <div class="rbt-table-responsive-cell-content">
-                            <button class="btn btn-sm btn-outline-primary">Reset</button>
+                            <button class="btn btn-sm btn-outline-primary demojs-showGrowl">Reset</button>
                         </div></td>
                 </tr>
                 <tr>
@@ -53,23 +52,36 @@ $page_title = 'Reset user verifications';
                     <td><b class="rbt-table-responsive-cell-label">Type</b> <span class="rbt-table-responsive-cell-content ">Verify</span></td>
                     <td><b class="rbt-table-responsive-cell-label visible-xs"> Actions </b>
                         <div class="rbt-table-responsive-cell-content">
-                            <button class="btn btn-sm btn-outline-primary">Reset</button>
+                            <button class="btn btn-sm btn-outline-primary demojs-showGrowl">Reset</button>
                         </div></td>
                 </tr>
             </tbody>
         </table>
+        <growls>
+            <div>
+                <alert dismissible="true">
+                    <div role="alert" class="alert alert-success alert-dismissible" style="display:none">
+                        <button type="button" class="close" aria-label="Close" data-dismiss="alert"> <span class="rbt-icon-close"></span> </button>
+                        <span>All verifications have now been reset for user '<span class="font-weight-bold">thrclark</span>'.</span> </div>
+                </alert>
+            </div>
+        </growls>
     </main>
 </div>
 <?php include('includes/all-footerscripts.php') ?>
-	
-	
-	
 <script>
     $(document).ready(function() {
         $("#search").click(function() {
             $(".demojs-searchresults").show();
         });
     }); 
-</script> 
+</script>
 </body>
 </html>
+<script>
+    $(document).ready(function() {
+        $(".demojs-showGrowl").click(function() {
+            $("growls .alert").fadeIn().delay(5000).fadeOut();
+        });
+    });
+</script>
