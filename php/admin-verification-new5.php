04f5362e8e95d765d2e196eba749a3a77e5d5a73
thrclark <thrclark@users.noreply.github.com>
2020-09-10 08:22:37 -0400

    INT-408

--- php/admin-verification-new5.php
+++ php/admin-verification-new5.php
@@ -23,14 +23,14 @@ $page_subtitle = 'Scheduling';
         <div class="row">
             <div class="col-2 border-right d-none d-sm-block">
                 <div id="sticky">
-                    <ol class="rvb-steps rvb-steps--vertical mb-5" style="overflow-x:inherit">
+                    <ol class="rvb-steps rvb-steps--vertical mb-7" style="overflow-x:inherit">
                         <li class="rvb-steps__item"> <a href="admin-verification-new1.php" class="rvb-steps__item-content"> <span class="rvb-steps__label  d-none d-lg-block">Setup</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-gear"></i></span> </a> </li>
                         <li class="rvb-steps__item"> <a href="admin-verification-new2.php" class="rvb-steps__item-content"> <span class="rvb-steps__label  d-none d-lg-block">Data</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-clipboard"></i></span> </a></li>
                         <li class="rvb-steps__item"> <a href="admin-verification-new3.php" class="rvb-steps__item-content"> <span class="rvb-steps__label  d-none d-lg-block">Recipients</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-users-group-solid"></i> </span> </a></li>
                         <li class="rvb-steps__item"> <a href="admin-verification-new5.php" class="rvb-steps__item-content" aria-current="step"><span class="rvb-steps__item-content" aria-current="step"> <span class="rvb-steps__label  d-none d-lg-block">Scheduling</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-calendar"></i></span> </span></a> </li>
                         <li class="rvb-steps__item"> <a href="admin-verification-new6.php" class="rvb-steps__item-content"> <span class="rvb-steps__label  d-none d-lg-block">Review/save</span> <span class="rvb-steps__indicator"> <i class="rbt-icon-eye"></i></span> </a> </li>
                     </ol>
-                </div>
+                    <a href="verification-preview.php" target="_blank" class="btn btn-link p-0"> Preview <i class="rbt-icon-link-external"></i> </a> </div>
             </div>
             <div class="col-12 col-sm-10 pl-sm-5">
                 <h2 class="mb-3 rbt-ts-20 rbt-ts-26-md-up"><?php echo $page_subtitle; ?></h2>
